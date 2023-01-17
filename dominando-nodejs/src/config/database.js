eb61ace0642fc3187690205ae21a288d0a5a6d86
Marcal <marcalsantamre@gmail.com>
2023-01-16 21:48:59 -0300

    Criada migration para a tabela Customer. Criado o model para a tabela Customer.

--- dominando-nodejs/src/config/database.js
+++ dominando-nodejs/src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
   dialect: "postgres",
   host: "localhost",
   username: "postgres",
-  password: "secret",
+  password: "1234",
   database: "dominando-nodejs",
   define: {
     timestamp: true,
